7a3b3854bbb7bc478e70b4a4047fedb7b19d2034
veppev <vepppev5@gmail.com>
2017-07-17 09:34:40 +0300

    Add hash-code and change mod of name.

--- src/source/DB/DBUser.kt
+++ src/source/DB/DBUser.kt
@@ -2,9 +2,11 @@ package source.DB
 
 import java.sql.Timestamp
 
-data class DBUser(val login: String, var password: String, val name: String, var lastVisit: Timestamp?) {
+data class DBUser(val login: String, var password: String, var name: String, var lastVisit: Timestamp?) {
     override fun equals(other: Any?): Boolean {
         if (other !is DBUser) return false
         return (login == other.login && password == other.password && name == other.name)
     }
+
+    override fun hashCode() = login.hashCode()
 }
