252aee88b111fa85a9cb2e231d6fe23f4f0c9efa
veppev <vepppev5@gmail.com>
2017-07-18 03:04:57 +0300

    start changing

--- src/source/DB/DBInterface.kt
+++ src/source/DB/DBInterface.kt
@@ -2,6 +2,7 @@ package source.DB
 
 interface DBInterface {
     fun loadUser(login: String): DBUser? //++
+    fun DBUser.conversations(): Set<DBConversation>
     fun loadConversations(user: DBUser): Set<DBConversation> //++
     fun loadMessages(conversation: DBConversation, count: Int): List<DBMessage> //++
     fun loadUsers(conversation: DBConversation): Set<DBUser>
